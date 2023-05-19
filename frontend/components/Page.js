5838e2b7248366cbba23c6124139125709ae5504
mrdom2 <pedroasd35@gmail.com>
2023-05-19 20:35:29 +0300

    front

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -39,7 +39,7 @@ const GlobalStyles = createGlobalStyle`
     text-decoration: underline;
   }
   button {
-    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue';
   }
 `;
 
